42aae17ce148ef53805dc08f5842bd7ea3c1923a
marietegr11 <marieteguerra@gmail.com>
2022-04-12 17:31:51 +0200

    primeras interfaces de login

---
+++ app/src/main/java/com/example/just_scan/LogAdmin.java
@@ -0,0 +1,14 @@
+package com.example.just_scan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_admin);
+    }
+}
---
+++ app/src/main/java/com/example/just_scan/LogRecuperarCuenta.java
@@ -0,0 +1,14 @@
+package com.example.just_scan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogRecuperarCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_recuperar_cuenta);
+    }
+}
