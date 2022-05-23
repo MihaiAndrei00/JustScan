2a1bff2f0b3cd94f53007240abfe1eb798c762c1
marietegr11 <marieteguerra@gmail.com>
2022-05-23 18:50:15 +0200

    qr implementado

--- app/src/main/java/com/example/controladores/Qr.java
+++ app/src/main/java/com/example/controladores/Qr.java
@@ -1,70 +1,155 @@
 package com.example.controladores;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.webkit.URLUtil;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.example.just_scan.R;
 
+import java.io.IOException;
+
 public class Qr extends AppCompatActivity {
-    private Button btn_escanear;
-    private TextView txt_res;
-    //ayuda la integracion de los escaneos mediante intents y recibe un resultado
-    private IntentIntegrator integrador;
-    //el IntentResult es el resultado que recibimos del codigo qr o de barras
-    private IntentResult result;
+    private CameraSource cameraSource;
+    private SurfaceView cameraView;
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private String token = "";
+    private String tokenanterior = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
 
-        btn_escanear=findViewById(R.id.btn_escanear);
-        txt_res=findViewById(R.id.txt_res);
+        cameraView = (SurfaceView) findViewById(R.id.camera_view);
+        initQR();
 
-        btn_escanear.setOnClickListener(view -> {
-            integrador=new IntentIntegrator(Qr.this);
-            //personalizamos el lector
-
-            //le establezco los tipos de codigos que puede leer , que le he estabecido a todos
-            integrador.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
-            //titulo que aparece en el lector
-            integrador.setPrompt("Escanealo!");
-            // se refiere a que camara va a usar (la 0 es la trasera)
-            integrador.setCameraId(0);
-            //hace un sonido cuando lea el codigo
-            integrador.setBeepEnabled(true);
-            //para que pueda leer codigos de barras
-            integrador.setBarcodeImageEnabled(true);
-            //para que se inicie el escaneo
-            integrador.initiateScan();
-
-        });
     }
 
-    //
-    protected void onActivityResult(int requestCode,int resultCode, Intent data) {
-        //creamos un obejto de tipo IntentResult que le pasamos lo que recibimos en el metodo
-        result= IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-        //le indicamos que si el result no es nulo recibes informacion del qr o codigo de barras
-        if (result!=null){
-            if (result.getContents()==null){
-                Toast.makeText(this, "Lectura cancelada", Toast.LENGTH_LONG).show();
-            }else{
-                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
-                txt_res.setText(result.getContents());
+    public void initQR() {
+
+        // creo el detector qr
+        BarcodeDetector barcodeDetector =
+                new BarcodeDetector.Builder(this)
+                        .setBarcodeFormats(Barcode.ALL_FORMATS)
+                        .build();
+
+        // creo la camara
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1600, 1024)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+
+        // listener de ciclo de vida de la camara
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                // verifico si el usuario dio los permisos para la camara
+                if (ActivityCompat.checkSelfPermission(Qr.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        // verificamos la version de ANdroid que sea al menos la M para mostrar
+                        // el dialog de la solicitud de la camara
+                        if (shouldShowRequestPermissionRationale(
+                                Manifest.permission.CAMERA)) ;
+                        requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                MY_PERMISSIONS_REQUEST_CAMERA);
+                    }
+                    return;
+                } else {
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException ie) {
+                        Log.e("CAMERA SOURCE", ie.getMessage());
+                    }
+                }
             }
-        }else{
-            super.onActivityResult(requestCode, resultCode, data);
 
-        }
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        // preparo el detector de QR
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() > 0) {
+
+                    // obtenemos el token
+                    token = barcodes.valueAt(0).displayValue.toString();
+
+                    // verificamos que el token anterior no se igual al actual
+                    // esto es util para evitar multiples llamadas empleando el mismo token
+                    if (!token.equals(tokenanterior)) {
+
+                        // guardamos el ultimo token proceado
+                        tokenanterior = token;
+                        Log.i("token", token);
+
+                        if (URLUtil.isValidUrl(token)) {
+                            // si es una URL valida abre el navegador
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(token));
+                            startActivity(browserIntent);
+                        } else {
+                            // comparte en otras apps
+                            Intent shareIntent = new Intent();
+                            shareIntent.setAction(Intent.ACTION_SEND);
+                            shareIntent.putExtra(Intent.EXTRA_TEXT, token);
+                            shareIntent.setType("text/plain");
+                            startActivity(shareIntent);
+                        }
+
+                        new Thread(new Runnable() {
+                            public void run() {
+                                try {
+                                    synchronized (this) {
+                                        wait(5000);
+                                        // limpiamos el token
+                                        tokenanterior = "";
+                                    }
+                                } catch (InterruptedException e) {
+                                    // TODO Auto-generated catch block
+                                    Log.e("Error", "Waiting didnt work!!");
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
+
+                    }
+                }
+            }
+        });
+
     }
+
 }
